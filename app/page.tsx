10bc4637ba968d776b16f1fff4f92b8687cb364c
Rokas <rookas.rudzenskas@gmail.com>
2023-09-30 18:59:26 +0300

    remote

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 // import HeroCarousel from "@/components/HeroCarousel"
 // import Searchbar from "@/components/Searchbar"
-// import Image from "next/image"
 import { getAllProducts } from "@/lib/actions"
+import Image from "next/image";
 // import ProductCard from "@/components/ProductCard"
 
 const Home = async () => {
@@ -10,36 +10,36 @@ const Home = async () => {
   return (
     <>
       <section className="px-6 md:px-20 py-24">
-        {/*<div className="flex max-xl:flex-col gap-16">*/}
-        {/*  <div className="flex flex-col justify-center"> */}
-      {/*      <p className="small-text">*/}
-      {/*        Smart Shopping Starts Here:*/}
-      {/*        <Image */}
-      {/*          src="/assets/icons/arrow-right.svg"*/}
-      {/*          alt="arrow-right"*/}
-      {/*          width={16}*/}
-      {/*          height={16}*/}
-      {/*        />*/}
-      {/*      </p>*/}
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">
+              Smart Shopping Starts Here:
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
 
-      {/*      <h1 className="head-text">*/}
-      {/*        Unleash the Power of*/}
-      {/*        <span className="text-primary"> PriceWise</span>*/}
-      {/*      </h1>*/}
+            <h1 className="head-text">
+              Unleash the Power of
+              <span className="text-primary"> Tracker App</span>
+            </h1>
 
-      {/*      <p className="mt-6">*/}
-      {/*        Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.*/}
-      {/*      </p>*/}
+            <p className="mt-6">
+              Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
+            </p>
 
       {/*      <Searchbar />*/}
-      {/*    </div>*/}
+          </div>
 
-      {/*    <HeroCarousel />*/}
-      {/*  </div>*/}
-      {/*</section>*/}
+          {/*<HeroCarousel />*/}
+        </div>
+      </section>
 
-      {/*<section className="trending-section">*/}
-      {/*  <h2 className="section-text">Trending</h2>*/}
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
 
       {/*  <div className="flex flex-wrap gap-x-8 gap-y-16">*/}
       {/*    {allProducts?.map((product) => (*/}
