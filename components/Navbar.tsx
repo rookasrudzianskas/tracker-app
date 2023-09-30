75781efae91b390e547a765901a8077522879e2f
Rokas <rookas.rudzenskas@gmail.com>
2023-09-30 18:57:46 +0300

    fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = ({}) => {
           />
 
           <p className="nav-logo">
-            Price<span className='text-primary'>Wise</span>
+            Tracker<span className='text-primary'> App</span>
           </p>
         </Link>
 
