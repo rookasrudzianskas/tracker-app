ee71c60f6e0ef0e3c445fec087b684871329b76b
Rokas <rookas.rudzenskas@gmail.com>
2023-09-30 19:11:36 +0300

    fix

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -2,13 +2,34 @@
 
 import React, {useState} from 'react';
 
+const isValidAmazonProductURL = (url: string) => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+
+    if(
+      hostname.includes('amazon.com') ||
+      hostname.includes ('amazon.') ||
+      hostname.endsWith('amazon')
+    ) {
+      return true;
+    }
+  } catch (error) {
+    return false;
+  }
+
+  return false;
+}
+
+
 const Searchbar = ({}) => {
   const [searchPrompt, setSearchPrompt] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = (e:  React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("searching");
+
+    const isValidLink = isValidAmazonProductURL(searchPrompt);
   }
 
   return (
