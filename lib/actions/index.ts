2eeacc4f91cbb540857db6b1aabc2e3bae893a15
Rokas <rookas.rudzenskas@gmail.com>
2023-10-01 10:28:02 +0300

    fix

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -23,9 +23,30 @@ export async function scrapeAndStoreProduct(productUrl: string) {
 
     const existingProduct = await Product.findOne({ url: scrapedProduct.url });
 
+    if(existingProduct) {
+      const updatedPriceHistory: any = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice }
+      ]
 
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+      }
+    }
+
+    const newProduct = await Product.findOneAndUpdate(
+      { url: scrapedProduct.url },
+      product,
+      { upsert: true, new: true }
+    )
+
+    revalidatePath(`/products/${newProduct._id}`);
   } catch (error) {
-
+    throw new Error(`Failed to create/update product: ${error.message}`)
   }
 }
 
