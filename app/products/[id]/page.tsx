38beb6dcd1936c898c5777d9dddfe72d9afbf534
Rokas <rookas.rudzenskas@gmail.com>
2023-10-01 10:42:00 +0300

    fix

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,6 +1,24 @@
-import React from 'react';
+// import Modal from "@/components/Modal";
+// import PriceInfoCard from "@/components/PriceInfoCard";
+import ProductCard from "@/components/ProductCard";
+import { getProductById, getSimilarProducts } from "@/lib/actions"
+import { formatNumber } from "@/lib/utils";
+import { Product } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
-const ProductDetails = ({}) => {
+type Props = {
+  params: { id: string }
+}
+
+const ProductDetails = async ({ params: { id } }: Props) => {
+  const product: Product = await getProductById(id);
+
+  if(!product) redirect('/')
+
+  const similarProducts = await getSimilarProducts(id);
+
   return (
     <div>
 
